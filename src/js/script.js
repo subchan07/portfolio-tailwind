bda5de48f646cfbcc0ceeec1fa5862a8755dc954
subchan07 <muchammadsubchan07@gmail.com>
2024-03-24 10:28:41 +0700

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,32 @@
+// Navbar Fixed
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+  const backToTop = document.querySelector("#back-to-top");
+
+  if (window.scrollY > fixedNav) {
+    header.classList.add("navbar-fixed");
+    backToTop.classList.remove("hidden");
+    backToTop.classList.add("flex");
+  } else {
+    header.classList.remove("navbar-fixed");
+    backToTop.classList.add("hidden");
+    backToTop.classList.remove("flex");
+  }
+};
+
+// Hamburger
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
+});
+
+// Klick di luar hamburger
+window.addEventListener("click", function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
